933ea5f4057aba02928818965fc052a115f4a58f
Никита <nikitanut@gmail.com>
2020-01-12 23:12:01 +0300

    Updated AppSettings

---
+++ TelegramFootballBot/AppSettings.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace TelegramFootballBot
+{
+    public static class AppSettings
+    {
+        private static readonly IConfiguration _configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+        public static string BotToken => _configuration["botToken"];
+
+        public static string BotName => _configuration["botName"];
+
+        public static string GoogleDocSheetId => _configuration["googleDocSheetId"];
+
+        public static TimeSpan DistributionTime => TimeSpan.Parse(_configuration["distributionTime"]);
+
+        public static TimeSpan GameDay => TimeSpan.Parse(_configuration["gameDay"]);
+
+        public static int BotOwnerChatId => int.Parse(_configuration["BotOwnerChatId"]);
+    }
+}
