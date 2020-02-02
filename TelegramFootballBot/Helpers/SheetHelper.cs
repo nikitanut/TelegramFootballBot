ea396330c12f6ce99be159ccad7192b8ab67cd2d
Никита <nikitanut@gmail.com>
2020-02-02 13:28:43 +0300

    Set new dashed string format.

--- TelegramFootballBot/Helpers/SheetHelper.cs
+++ TelegramFootballBot/Helpers/SheetHelper.cs
@@ -204,7 +204,7 @@ namespace TelegramFootballBot.Helpers
 
         private static string GetDashedString(int mainStringLength)
         {
-            return new string('-', (int)(mainStringLength * 1.5));
+            return new string('-', 30);
         }
 
         private static CopyPasteRequest GetCopyStyleRequest(int sourceRowIndex, int destinationRowIndex)
