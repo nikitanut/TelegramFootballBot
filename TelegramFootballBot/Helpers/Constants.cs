32c7b4350b5a72a3baff41250c37d830e6d5ce3f
Никита <nikitanut@gmail.com>
2020-02-06 22:38:52 +0300

    Decreased CancellationToken timeout

--- TelegramFootballBot/Helpers/Constants.cs
+++ TelegramFootballBot/Helpers/Constants.cs
@@ -5,7 +5,7 @@
         public const string PLAYERS_SET_CALLBACK_PREFIX = "PlayersSetDetermination";
         public const char PLAYERS_SET_CALLBACK_PREFIX_SEPARATOR = '_';
         public const char CALLBACK_DATA_SEPARATOR = '|';
-        public const int ASYNC_OPERATION_TIMEOUT = 20000;
+        public const int ASYNC_OPERATION_TIMEOUT = 10000;
         public const string YES_ANSWER = "Да";
         public const string NO_ANSWER = "Нет";
         public const string MAYBE_ANSWER = "+ / -";
