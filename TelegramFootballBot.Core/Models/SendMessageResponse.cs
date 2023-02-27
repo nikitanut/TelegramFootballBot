5a94b228a278f84ed0f70e68d4e28f6e40cf1280
Nikita <nikitanut@gmail.com>
2023-02-27 22:15:25 +0400

    Process errors while sending messages

---
+++ TelegramFootballBot.Core/Models/SendMessageResponse.cs
@@ -0,0 +1,19 @@
+﻿using Telegram.Bot.Types;
+
+namespace TelegramFootballBot.Core.Models
+{
+    public class SendMessageResponse
+    {
+        public ChatId ChatId { get; set; }
+
+        public SendStatus Status { get; set; }
+
+        public string Message { get; set; }        
+    }
+
+    public enum SendStatus
+    {
+        Success = 1,
+        Error = 2
+    }
+}
